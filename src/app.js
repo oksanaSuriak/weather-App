9c2b518579a476a941c84eaac53167fc630047e6
oksanaSuriak <115495040+oksanaSuriak@users.noreply.github.com>
2022-10-22 18:36:38 +0300

    add search form

--- src/app.js
+++ src/app.js
@@ -34,10 +34,20 @@ iconElement.setAttribute(
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-
+function search(city) {
 let apiKey = "66c7da026329d4c2a98c508b6ddbcfb1";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value)
+ }
+
+search("New York")
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
