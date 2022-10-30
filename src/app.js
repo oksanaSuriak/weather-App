4f475859a04cb4ae2df42fe521f771082b4f9610
oksanaSuriak <115495040+oksanaSuriak@users.noreply.github.com>
2022-10-30 14:31:30 +0200

    removed the imperial conversion

--- src/app.js
+++ src/app.js
@@ -93,33 +93,10 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
  }
-
-function showFahrenheitTemperature(event){
-event.preventDefault();
-let temperatureElement = document.querySelector("#temperature");
-celsiusLink.classList.remove("active");
-fahrenheitLink.classList.add("active");
-let fahrenheitTemperature = (celsiusTemperature * 9) / 5 +32;
-temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
- function showCelsiusTemperature(event){
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
- }
-let celsiusTemperature = null;
-
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", showCelsiusTemperature);
+
 
 search("New York");
